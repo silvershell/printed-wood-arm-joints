fa69fff5c93764f2086497bce6ec8abd0e7467c3
silvershell <silvershell100@gmail.com>
2017-07-24 06:29:05 +0900

    Added config.js

---
+++ src/config.js
@@ -0,0 +1,145 @@
+let config = {
+    layer: 0.4,
+    t: 3,
+    cr: 2,
+    nut: {
+        enable: true,
+        d: 11.5 + 0.8,
+        h: 3.6,
+    },
+    nutS: {
+        enable: true,
+        // // M4
+        // d: 8.1 + 0.3,
+        // h: 3.2 + 0.4,
+        // M3
+        d: 6.4 + 0.4,
+        h: 2.4 + 0.4,
+    },
+    bar: {
+        hole: {
+            enable: true,
+        },
+        size: [40,40+0.8,30+0.6],
+    },
+    screw: {
+        d: 6 + 0.4,
+        fn: 16,
+    },
+    screwS: {
+        // // M4
+        // d: 4 + 0.4,
+        // M3
+        d: 3 + 0.6,
+        fn: 16,
+    },
+    centerScrewHole: {
+        enable: true,
+    },
+    sideScrewHole: {
+        enable: false,
+        d: 6 + 0.4,
+        fn: 16,
+    },
+    rotateJointModule: {
+        enable: true,
+        t: 4,
+        // d: 30,
+        // l: 20,
+        fn: 32,
+        screw: {
+            d: 6 + 0.4,
+            fn: 16,
+        },
+
+        num: 3,
+        margin: 0.4,
+
+        nutGrip: {
+            enable: false,
+            d: 11.5 + 0.8,
+            h: 3.6 + 1,
+            t: 2,
+            cr: 2,    // corner round r
+            crFn: 16,    // corner round fn
+            fn: 32,
+        },
+    },
+    mount: {
+        d: 24,
+        fn: 32,
+        l: 34,
+        t: 5,
+        bottomT: 4,
+        crfn: 16,
+        nutHoleH: 5 + 0.4,
+    },
+    woodScrewHole: {
+        enable: true,
+        d: 4 + 0.4,
+        h: 20,
+        fn: 16,
+        head: {
+            h: 2.3,
+            fn: 32,
+        }
+    },
+    spacer: {
+        d: 28,
+        h: 11,
+        fn: 64,
+    },
+    spacer2: {
+        h: 6,
+    },
+    attachment: {
+        d: 20,
+        h: 15,
+        fn: 32,
+        l: 40,
+        r: 2,
+    },
+    nutHolder: {
+        clip: {
+            r: 3,
+            size: [34,34,20],
+        }
+    },
+    arm: {
+        l: 20,
+        disable: true,
+    },
+    rotateArm: {
+        h: 15+20,
+    },
+    smartphone: {
+        t: 16-2,
+        w: 70+1.6*2,
+        margin: 10,
+        clipArm: {
+            w: 8.4 + 2*2,
+            t: 3,
+            fn: 16,
+            margin: 95,
+            wingFrontL: 7,
+            wingBackL: 20,
+            marginY: 16,
+            mount: {
+                t: 7,
+                w: 8.4 + 2*2,
+                wing: 3,
+            },
+            nutT: 1.6,
+        },
+        centerBar: {
+            w: 11.5 + 3*2,
+            t: 5+4+1,
+            t2: 5,
+            r: 1,
+            nutT: 1.6,
+        },
+    },
+}
+
+
+module.exports = config
--- src/printed-wood-arm-joints.js
+++ src/printed-wood-arm-joints.js
@@ -7,6 +7,10 @@ const {
 
 const _ = require('lodash');
 
+
+let printedWoodArmJointParam = require('./config.js');
+
+
 // ------------
 
 function roundCube(p){
@@ -441,150 +445,6 @@ function boltHandle(opts){
 
 // -----------------------------------
 
-let printedWoodArmJointParam = {
-    layer: 0.4,
-    t: 3,
-    cr: 2,
-    nut: {
-        enable: true,
-        d: 11.5 + 0.8,
-        h: 3.6,
-    },
-    nutS: {
-        enable: true,
-        // // M4
-        // d: 8.1 + 0.3,
-        // h: 3.2 + 0.4,
-        // M3
-        d: 6.4 + 0.4,
-        h: 2.4 + 0.4,
-    },
-    bar: {
-        hole: {
-            enable: true,
-        },
-        size: [40,40+0.8,30+0.6],
-    },
-    screw: {
-        d: 6 + 0.4,
-        fn: 16,
-    },
-    screwS: {
-        // // M4
-        // d: 4 + 0.4,
-        // M3
-        d: 3 + 0.6,
-        fn: 16,
-    },
-    centerScrewHole: {
-        enable: true,
-    },
-    sideScrewHole: {
-        enable: false,
-        d: 6 + 0.4,
-        fn: 16,
-    },
-    rotateJointModule: {
-        enable: true,
-        t: 4,
-        // d: 30,
-        // l: 20,
-        fn: 32,
-        screw: {
-            d: 6 + 0.4,
-            fn: 16,
-        },
-
-        num: 3,
-        margin: 0.4,
-
-        nutGrip: {
-            enable: false,
-            d: 11.5 + 0.8,
-            h: 3.6 + 1,
-            t: 2,
-            cr: 2,    // corner round r
-            crFn: 16,    // corner round fn
-            fn: 32,
-        },
-    },
-    mount: {
-        d: 24,
-        fn: 32,
-        l: 34,
-        t: 5,
-        bottomT: 4,
-        crfn: 16,
-        nutHoleH: 5 + 0.4,
-    },
-    woodScrewHole: {
-        enable: true,
-        d: 4 + 0.4,
-        h: 20,
-        fn: 16,
-        head: {
-            h: 2.3,
-            fn: 32,
-        }
-    },
-    spacer: {
-        d: 28,
-        h: 11,
-        fn: 64,
-    },
-    spacer2: {
-        h: 6,
-    },
-    attachment: {
-        d: 20,
-        h: 15,
-        fn: 32,
-        l: 40,
-        r: 2,
-    },
-    nutHolder: {
-        clip: {
-            r: 3,
-            size: [34,34,20],
-        }
-    },
-    arm: {
-        l: 20,
-        disable: true,
-    },
-    rotateArm: {
-        h: 15+20,
-    },
-    smartphone: {
-        t: 16-2,
-        w: 70+1.6*2,
-        margin: 10,
-        clipArm: {
-            w: 8.4 + 2*2,
-            t: 3,
-            fn: 16,
-            margin: 95,
-            wingFrontL: 7,
-            wingBackL: 20,
-            marginY: 16,
-            mount: {
-                t: 7,
-                w: 8.4 + 2*2,
-                wing: 3,
-            },
-            nutT: 1.6,
-        },
-        centerBar: {
-            w: 11.5 + 3*2,
-            t: 5+4+1,
-            t2: 5,
-            r: 1,
-            nutT: 1.6,
-        },
-    },
-}
-
-
 function spacer1(options){
     let p = _.cloneDeep(printedWoodArmJointParam)
     _.merge(p, options);
@@ -711,11 +571,6 @@ function printedWoodArmJointBase(options){
     ]
 
     let solid = [
-        // roundCube({
-        //     r: p.t,
-        //     size: roundCubeSize,
-        // }),
-
         hullCube({
             r: p.t,
             size: roundCubeSize,
@@ -795,7 +650,6 @@ function printedWoodArmJointBase(options){
             translate([
                 -p.rotateJointModule.l+p.t, 
                 (p.bar.size[1]+p.t*2)/2 - (p.rotateJointModule.t+p.rotateJointModule.margin)*(p.rotateJointModule.num-1),
-                // 0
                 p.rotateJointModule.d/2
             ],[
                 rotate([-90,-90,0],[
@@ -829,14 +683,6 @@ function printedWoodArmJointBase(options){
                 [p.t+p.bar.size[0]/4, p.t+p.bar.size[1]*3/4, 0],
                 [0,0,0],
             ],
-            // [
-            //     [p.t+p.bar.size[0]/4, p.t+p.bar.size[1]/4, 0],
-            //     [0,0,0],
-            // ],
-            // [
-            //     [p.t+p.bar.size[0]*3/4, p.t+p.bar.size[1]*3/4, 0],
-            //     [0,0,0],
-            // ],
             [
                 [p.t+p.bar.size[0]/4, p.t+p.bar.size[1]/4, roundCubeSize[2]],
                 [0,180,0],
@@ -1372,9 +1218,7 @@ function smartPhoneGrip2(opts){
         ]),
         ]),
     ])
-
-    // console.log(JSON.stringify(m, null, "  "));
-
+    
     return m
 }
 
